894f1ead4ae976d8d9c1e0aa2cff277fc909265a
UmutSanci01 <umutsanci01@gmail.com>
2026-06-25 16:45:38 +0300

    The supplier retains its last fuel level.

--- Supplier/Supplier.gd
+++ Supplier/Supplier.gd
@@ -14,15 +14,19 @@ onready var timer = $Timer
 
 
 var is_active : bool = false
+var is_fueled : bool = false # Had this Supplier been fueled before save.
 
 
 func _ready():
+	add_to_group("save_data")
 	timer.wait_time = product_time
+
+	load_data()
 
 
 func enable():
 #	if PlayerInventory.get_item_amount(product_source) < product_cost:
-	if not timer.time_left and not PlayerInventory.use_item(product_source):
+	if not is_fueled and not timer.time_left and not PlayerInventory.use_item(product_source):
 		InfoPanel.add_label("Yeteri kadar malzeme yok.")
 		return false
 	
@@ -45,6 +49,20 @@ func disable():
 	else:
 		timer.stop()
 
+func save_data():
+	var data : Dictionary = {"time_left" : timer.time_left}
+	DataBase.save_data(data, "supplier" + self.name)
+
+func load_data():
+	var data = DataBase.load_data("supplier" + self.name)
+	if data.empty():
+		return 0
+	
+	if data.has("time_left"):
+		var left = data["time_left"]
+		if left > 0:
+			timer.wait_time = left
+			is_fueled = true
 
 func _on_Timer_timeout():
 	PlayerInventory.add_item(product_item, product_num)
@@ -53,3 +71,7 @@ func _on_Timer_timeout():
 		InfoPanel.add_label("Hammadde Tükendi", "", Color.tomato)
 		timer.stop()
 		disable()
+
+	if timer.wait_time != product_time:
+		is_fueled = false
+		timer.start(product_time)
