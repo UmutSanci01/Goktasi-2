dbc5fdf25a384e6fc1e2f488dcb7e3c6f5b82d9c
UmutSanci01 <umutsanci01@gmail.com>
2026-09-05 20:11:12 +0300

    Clear comments

--- GUI/Map/InfoPanel/InfoPanel.gd
+++ GUI/Map/InfoPanel/InfoPanel.gd
@@ -4,31 +4,12 @@ onready var label_ore_num = $PanelContainer/VBoxContainer/OreNum/Value
 onready var label_radius = $PanelContainer/VBoxContainer/Radius/Value
 onready var label_dist = $PanelContainer/VBoxContainer/Dist/Value
 
-#var screen_size : Vector2 = OS.get_screen_size()
-#var screen_size : Vector2 = OS.get_real_window_size()
-#var screen_size : Vector2 = OS.window_size
 var screen_size : Vector2
 var screen_size_half : Vector2
-
-#func _draw():
-#	draw_circle(screen_size_half, 10, Color.purple)
-#	draw_circle(screen_size, 10, Color.red)
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	screen_size_half = screen_size / 2
-	
-#	show()
-#	var scale : Vector2
-#	scale.x = screen_size.x / 683
-#	scale.y = screen_size.y / 768
-#	print(screen_size)
-#	var size : Vector2
-#	size.x = screen_size.x / 4
-#	size.y = 128
-	
-#	$PanelContainer.set_deferred("rect_size", size)
-#	set_deferred("rect_scale", scale)
 
 func pop(base_pos: Vector2, slot_scaled_size: Vector2) -> void:
 	show()
@@ -36,21 +17,19 @@ func pop(base_pos: Vector2, slot_scaled_size: Vector2) -> void:
 	var direction: Vector2 = Vector2.ONE
 	var offset: Vector2 = Vector2.ZERO
 	
-	# X Ekseni Kontrolü
 	if base_pos.x < screen_size_half.x:
 		direction.x = 1
-		offset.x = slot_scaled_size.x # Slot ekranın solundaysa paneli slotun genişliği kadar sağa it
+		offset.x = slot_scaled_size.x
 	else:
 		direction.x = -1
-		offset.x = $PanelContainer.rect_size.x # Slot sağdaysa paneli kendi genişliği kadar sola çek
+		offset.x = $PanelContainer.rect_size.x
 	
-	# Y Ekseni Kontrolü
 	if base_pos.y < screen_size_half.y:
 		direction.y = 1
-		offset.y = slot_scaled_size.y # Slot ekranın üstündeyse paneli slotun yüksekliği kadar aşağı it
+		offset.y = slot_scaled_size.y
 	else:
 		direction.y = -1
-		offset.y = $PanelContainer.rect_size.y # Slot altındaysa paneli kendi yüksekliği kadar yukarı çek
+		offset.y = $PanelContainer.rect_size.y
 	
 	rect_position = base_pos + (offset * direction)
 
